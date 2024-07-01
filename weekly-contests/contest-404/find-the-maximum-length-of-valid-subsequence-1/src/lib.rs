f593be8fca761b0df66c4864c2477e9b184f6099
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-07-01 00:29:23 -0500

    Updated contest 404 q2
    Formatted the code.

--- weekly-contests/contest-404/find-the-maximum-length-of-valid-subsequence-1/src/lib.rs
+++ weekly-contests/contest-404/find-the-maximum-length-of-valid-subsequence-1/src/lib.rs
@@ -1,9 +1,12 @@
 //! You are given an integer array nums.
 //! A `subsequence` sub of nums with length x is called valid if it satisfies:
-//! - `(sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] + sub[x - 1]) % 2.`
+//! - `(sub[0] + sub[1]) % 2 == (sub[1] + sub[2]) % 2 == ... == (sub[x - 2] +
+//!   sub[x - 1]) % 2.`
 //!
 //! Return the length of the longest valid subsequence of nums.
-//! A `subsequence` is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
+//! A `subsequence` is an array that can be derived from another array by
+//! deleting some or no elements without changing the order of the remaining
+//! elements.
 
 pub fn maximum_length(nums: Vec<i32>) -> i32 {
     -1
