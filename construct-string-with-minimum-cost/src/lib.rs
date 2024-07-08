7175915092dc22bd66d2c2ef56b10ee8a541e59a
Fangjun Zhou <fangjun.zhou02@gmail.com>
2024-07-08 10:15:40 -0500

    Added LeetCode 3213

---
+++ construct-string-with-minimum-cost/src/lib.rs
@@ -0,0 +1,14 @@
+//! [LeetCode 3213](https://leetcode.com/problems/construct-string-with-minimum-cost/)
+
+pub fn minimum_cost(
+    target: String,
+    words: Vec<String>,
+    costs: Vec<i32>,
+) -> i32 {
+    return -1;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
